ee90db510fdf2b096c4ce5156d3a7413b9cffc34
Lyudmil Ivanov <lyudmil.ivanov@protonmail.com>
2022-01-21 17:38:54 +0200

    working contract - singleton

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,51 +1,7 @@
-import { Context, logging, RNG, ContractPromiseBatch, u128, context } from "near-sdk-as"
-import { Contract } from "../../singleton/assembly";
-import { ONE_NEAR, XCC_GAS, assert_self, assert_single_promise_success } from "../../utils";
+import { Context } from "near-sdk-as"
 
-// get the current execution block id 
+// TEST GROUND
+
 export function get_block(): string {
   return `The current execution block is ${Context.blockIndex} use that information wisely.`
 }
-
-// guess the tx execution block id 
-export function guess_block(guessed_block_index: u64): string {
-  logging.log(`sender ${Context.sender}`)
-  logging.log(`balance of sender  ${Context.accountBalance}`)
-  logging.log(`contract name ${Context.contractName}`)
-
-  // for clarification
-  // const actual_block_index = Context.blockIndex
-  const actual_block_index = 11
-
-  if (guessed_block_index == actual_block_index) {
-    // reward_sender()
-
-    return `Hoaa you've guessed correctly ${Context.blockIndex} you shall be rewarded!`
-  } else {
-    return `Sorry, you ${missed_by(guessed_block_index, actual_block_index)} Better luck next time!`
-  }
-}
-
-// calculates "the missed by blocks" and returns a string
-function missed_by(guessed_block: u64, actual_block: u64): string {
-  const block_difference = abs(guessed_block as i64 - actual_block as i64)
-
-  if (guessed_block > actual_block) {
-    return `overshot by ${block_difference} blocks!`
-  } else {
-    return `undershot by ${block_difference} blocks!`
-  }
-}
-
-function reward_sender(): void {
-  ContractPromiseBatch
-    .create("ethuil.testnet")
-    .transfer(ONE_NEAR)
-}
-
-// function on_transfer_complete(): void {
-//   assert_self()
-//   assert_single_promise_success()
-
-//   logging.log("transfer complete")
-// }
--- src/singleton/assembly/index.ts
+++ src/singleton/assembly/index.ts
@@ -1,68 +1,47 @@
-import { storage, Context } from "near-sdk-core"
+import { Context, logging, ContractPromiseBatch, u128, } from "near-sdk-as"
+import { ONE_NEAR, XCC_GAS } from "../../utils";
+
 
 @nearBindgen
 export class Contract {
-  private message: string = 'hello world'
-
-  // return the string 'hello world'
-  helloWorld(): string {
-    return this.message
+  // returns the execution block height
+  get_block(): string {
+    return `the current block index is ${Context.blockIndex} use that information wisely`
   }
 
-  // read the given key from account (contract) storage
-  read(key: string): string {
-    if (isKeyInStorage(key)) {
-      return `✅ Key [ ${key} ] has value [ ${storage.getString(key)!} ] and "this.message" is [ ${this.message} ]`
+  // guess the tx execution block id 
+  guess_block(guessed_block_index: u64): string {
+    // for clarification
+    // const actual_block_index = Context.blockIndex
+    const actual_block_index = 11; // shortcircuit
+
+    if (guessed_block_index == actual_block_index) {
+      this.reward_sender()
+      return `Whoaa! you've guessed correctly ${Context.blockIndex} you shall be rewarded!`
     } else {
-      return `🚫 Key [ ${key} ] not found in storage. ( ${this.storageReport()} )`
+      return `Sorry, you ${this.missed_by(guessed_block_index, actual_block_index)} Better luck next time!`
     }
   }
 
-  /**
-  write the given value at the given key to account (contract) storage
-  ---
-  note: this is what account storage will look like AFTER the write() method is called the first time
-  ╔════════════════════════════════╤══════════════════════════════════════════════════════════════════════════════════╗
-  ║                            key │ value                                                                            ║
-  ╟────────────────────────────────┼──────────────────────────────────────────────────────────────────────────────────╢
-  ║                          STATE │ {                                                                                ║
-  ║                                │   "message": "data was saved"                                                    ║
-  ║                                │ }                                                                                ║
-  ╟────────────────────────────────┼──────────────────────────────────────────────────────────────────────────────────╢
-  ║                       some-key │ some value                                                                       ║
-  ╚════════════════════════════════╧══════════════════════════════════════════════════════════════════════════════════╝
-   */
-  @mutateState()
-  write(key: string, value: string): string {
-    storage.set(key, value)
-    this.message = 'data was saved' // this is why we need the deorator @mutateState() above the method name
-    return `✅ Data saved. ( ${this.storageReport()} )`
+  // calculates "the missed by blocks" :: STRING
+  private missed_by(guessed_block: u64, actual_block: u64): string {
+    const block_difference = abs(guessed_block - actual_block)
+
+    if (guessed_block > actual_block) {
+      return `overshot by ${block_difference} blocks!`
+    } else {
+      return `undershot by ${block_difference} blocks!`
+    }
   }
 
+  private reward_sender(): void {
+    const self = Context.contractName
 
-  // private helper method used by read() and write() above
-  private storageReport(): string {
-    return `storage [ ${Context.storageUsage} bytes ]`
+    ContractPromiseBatch.create(Context.sender)
+      .transfer(ONE_NEAR)
+      .then(self).function_call("on_reward_complete", "{}", u128.Zero, XCC_GAS);
   }
-}
-
-/**
- * This function exists only to avoid a compiler error
- *
-
-ERROR TS2339: Property 'contains' does not exist on type 'src/singleton/assembly/index/Contract'.
-
-     return this.contains(key);
-                 ~~~~~~~~
- in ~lib/near-sdk-core/storage.ts(119,17)
-
-/Users/sherif/Documents/code/near/_projects/edu.t3/starter--near-sdk-as/node_modules/asbuild/dist/main.js:6
-        throw err;
-        ^
-
- * @param key string key in account storage
- * @returns boolean indicating whether key exists
- */
-function isKeyInStorage(key: string): bool {
-  return storage.hasKey(key)
+  private on_reward_complete(): void {
+    logging.log("reward successful");
+  }
 }
